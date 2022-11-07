0cd49b054a8d41b759ad97d00a8d2de18cd5812f
Michel Metran <michelmetran@gmail.com>
2022-11-06 23:52:19 -0300

    s

--- setup/apps_codes.sh
+++ setup/apps_codes.sh
@@ -117,6 +117,7 @@ while true; do
         wget -O ~/Downloads/jetbrains-toolbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.25.12627.tar.gz";
         cd /opt/;
         tar -zxvf ~/Downloads/jetbrains-toolbox.tar.gz;
+        mv jetbrains-toolbox-1.25.12627/ jetbrains-toolbox/;
         jetbrains-toolbox/jetbrains-toolbox;
         rm ~/Downloads/jetbrains-toolbox.tar.gz;
         break;;
